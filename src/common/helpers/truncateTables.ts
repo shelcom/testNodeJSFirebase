d98759b78acb0a3c712bb70ab601fb0299e09b9d
Serhii Shelkovskyi <>
2024-09-24 15:37:56 +0200

    fix

--- src/common/helpers/truncateTables.ts
+++ src/common/helpers/truncateTables.ts
@@ -8,14 +8,14 @@ const db = knex(config);
 const logger = new LoggerService();
 
 export async function truncateTables(tables: string[]) {
-  jest.clearAllMocks();
+  // jest.clearAllMocks();
 
-  try {
-    const tablesList = tables.join(', ');
-    await db.raw(`TRUNCATE TABLE ${tablesList} RESTART IDENTITY CASCADE`);
-    logger.log(`Tables ${tablesList} truncated successfully.`);
-  } catch (error) {
-    logger.error('Error truncating tables:', error);
-    throw error;
-  }
+  // try {
+  //   const tablesList = tables.join(', ');
+  //   await db.raw(`TRUNCATE TABLE ${tablesList} RESTART IDENTITY CASCADE`);
+  //   logger.log(`Tables ${tablesList} truncated successfully.`);
+  // } catch (error) {
+  //   logger.error('Error truncating tables:', error);
+  //   throw error;
+  // }
 }
