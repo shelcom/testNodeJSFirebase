6ae25b6158c3e4863e88907d787e09385aadd68e
Sergio <44430546+shelcom@users.noreply.github.com>
2024-09-24 17:21:38 +0200

    fix

--- src/infrastructure/database/knex.config.ts
+++ src/infrastructure/database/knex.config.ts
@@ -3,17 +3,16 @@ import * as path from 'path';
 import 'dotenv/config';
 
 const useSSL = process.env.DB_SSL === 'true';
-const tt = 'AVNSkIP6x0UzJKXqSS6PZJ1'
 
 const knexConfig: {[key: string]: Knex.Config} = {
   development: {
     client: 'pg', // Ensure this client is set
     connection: {
-      host: process.env.DB_HOST || "pg-d109371-template-nest-js.c.aivencloud.com",
-      port: parseInt(process.env.DB_PORT || "14358", 10),
-      user: process.env.DB_USER || "avnadmin",
-      password: process.env.DB_PASSWORD || tt,
-      database: process.env.DB_NAME || "defaultdb",
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       ssl: useSSL ? {rejectUnauthorized: false} : false,
     },
     // migrations: {
@@ -21,28 +20,28 @@ const knexConfig: {[key: string]: Knex.Config} = {
     //   directory: path.resolve(__dirname, 'migrations'),
     // },
   },
-  test: {
-    client: 'pg', // Ensure this client is set
-    connection: {
-      host: process.env.DB_TEST_HOST || process.env.DB_HOST,
-      port:
-        parseInt(process.env.DB_TEST_PORT, 10) ||
-        parseInt(process.env.DB_PORT, 10),
-      user: process.env.DB_TEST_USER || process.env.DB_USER,
-      password: process.env.DB_TEST_PASSWORD || process.env.DB_PASSWORD,
-      database: process.env.DB_TEST_NAME || process.env.DB_NAME,
-      ssl: useSSL ? {rejectUnauthorized: false} : false,
-    },
-    // migrations: {
-    //   tableName: 'knex_migrations',
-    //   directory: path.resolve(__dirname, 'migrations'),
-    // },
-    pool: {
-      min: 2,
-      max: 10,
-      acquireTimeoutMillis: 30000, // Increase acquire timeout
-    },
-  },
+  // test: {
+  //   client: 'pg', // Ensure this client is set
+  //   connection: {
+  //     host: process.env.DATABASE_HOST || process.env.DATABASE_HOST,
+  //     port:
+  //       parseInt(process.env.DB_TEST_PORT, 10) ||
+  //       parseInt(process.env.DB_PORT, 10),
+  //     user: process.env.DB_TEST_USER || process.env.DB_USER,
+  //     password: process.env.DB_TEST_PASSWORD || process.env.DB_PASSWORD,
+  //     database: process.env.DB_TEST_NAME || process.env.DB_NAME,
+  //     ssl: useSSL ? {rejectUnauthorized: false} : false,
+  //   },
+  //   // migrations: {
+  //   //   tableName: 'knex_migrations',
+  //   //   directory: path.resolve(__dirname, 'migrations'),
+  //   // },
+  //   pool: {
+  //     min: 2,
+  //     max: 10,
+  //     acquireTimeoutMillis: 30000, // Increase acquire timeout
+  //   },
+  // },
 };
 
 export default knexConfig;
